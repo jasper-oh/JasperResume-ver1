6224524f30c5eae8e64c168909a73bdf8b457acc
Jasper Oh <ohyj0906@gmail.com>
2024-12-31 21:15:44 -0800

    [Fix] Icon img not showing

--- src/components/common/social-contact/index.tsx
+++ src/components/common/social-contact/index.tsx
@@ -2,22 +2,38 @@ import { useEffect, useState } from "react";
 import "./social-contact.css";
 import { Social } from "../../../data/repository/socialRepository";
 import { repositories } from "../../../data";
+import githubImg from "../../../assets/icons/github.png";
+import linkedinImg from "../../../assets/icons/linkedIn.png";
+import notionImg from "../../../assets/icons/notion.png";
 
 function SocialContact() {
-  const [social, setSocial] = useState<Social[]>([])
+  const [social, setSocial] = useState<Social[]>([]);
 
   useEffect(() => {
     const fetchSocial = async (): Promise<void> => {
       try {
         const socialData = await repositories.socialData.getAll();
-        setSocial(socialData);        
+        setSocial(socialData);
       } catch (error: any) {
         console.error(`Error fetching socials: ${error.message}`);
       }
     };
 
-    fetchSocial(); 
+    fetchSocial();
   }, []);
+
+  const getIcon = (icon: string) => {
+    switch (icon) {
+      case "notion.png":
+        return notionImg;
+      case "github.png":
+        return githubImg;
+      case "linkedIn.png":
+        return linkedinImg;
+      default:
+        return icon; 
+    }
+  };
 
   return (
     <div className="social-contact">
@@ -25,7 +41,7 @@ function SocialContact() {
         return (
           <a key={item.id} href={item.link}>
             <div className="social-icon-div">
-              <img src={"src/assets/icons/" + item.icon} className="social-icon" />
+              <img src={getIcon(item.icon)} className="social-icon" alt={item.icon} />
             </div>
           </a>
         );
